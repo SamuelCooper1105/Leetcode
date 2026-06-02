3aa7af9b0c9856115220591a949260476bea8649
Cooper Cumens <ccumens@controltouch.com>
2026-06-02 13:39:47 -0400

    please

--- med/substring.c
+++ med/substring.c
@@ -1,16 +1,35 @@
-struct hp {
-	int key;
-	int val;
-};
+int lengthOfLongestSubstring( char * s ) {
+    if(*s == '\0'){
+        return 0;
+    }
+        char * l = s;
+        char * r = l + 1;
+        int n = 1;
+        int max = 1;
+        char seen[256]={0};
+        seen[(unsigned char)*l] = 1;
+        
+        printf("Starting Point: \nl:%c | r:%c\n", *l, *r);
+        while(*r != '\0'){
+            int rdex = (unsigned char)(*r);
+            int ldex = (unsigned char)(*l);
 
-int hsh ( char c, int len ){
-	int n = c - '\0';
-	return len % n;
+            while( seen[rdex] == 1 ){
+                seen[ldex] = 0;
+                l++;
+                n--;
+                ldex = (unsigned char)*l;
+            }
+            n++;
+            if ( n > max ){
+                max = n;
+            }
+            printf("l:%c | r:%c | n:%d\n", *l, *r, n);
+            seen[rdex] = 1;
+            
+            r++;
+        }
+        printf("l: %s | r : %s\n", l, r);       
+        return max;
+
 }
-
-
-int substring ( ) {
-	
-
-}
-
