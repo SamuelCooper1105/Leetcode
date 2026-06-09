7e7d0ce212d330f944a9133acfd5f7a773ffbe98
Cooper Cumens <ccumens@controltouch.com>
2026-06-09 10:57:22 -0400

    added readme adn comments

---
+++ lrnchrd/README.md
@@ -0,0 +1,4 @@
+Hi and welcome to the subdirectory lrnchrdway, this directory is dedicated to the exercises within hte book Learn C the Hard way by Zed Shaw.
+
+none of this is my code, instead this is code I am reviewing to review c at its fundamentals and my comments on said code.
+
--- lrnchrd/ex8.c
+++ lrnchrd/ex8.c
@@ -5,9 +5,18 @@ int main ( int argc, char *argv[]){
     int areas[] = {10,10,12,13,14,20};
     char name [] = "Zed";
     char full_name[] = { 'Z', 'e', 'd', ' ', 'A', '.',' ', 'S','h','a','w','\0'};
-    printf("THe size of an int: %ld\n", sizeof(int));
+    printf("The size of an int(in bytes): %ld\n", sizeof(int));
+    //So here we see how large an int is in terms of bytes
+    //in c an integer type is 4 bytes, which gives it the range of 0 - 2^32, which is like 4 billion. 
     printf("The size of areas( int[]): %ld\n", sizeof(areas));
     printf("The number of ints in areas (int[]) : %ld\n", sizeof(areas)/sizeof(int));
+    //prior to this we determined that the size of areas is 24 which gives us the amount of
+    //btres the entire areas array holds in memory. This is not correct as we need
+    //to be able to have indexes to the array. We can solve this issue by simply taking 
+    //the size in bytes and dividing by the size of an indvidual element
+    //using areas as an example, we know that areas is an array of ints and we know the size
+    //of an int so we can simply divide the size of areas by the size of a single int, 
+    //which results in the amount of int types within the areas array. 
     printf("The first element of areas is %d and the second is %d", areas[0],areas[1]);
     printf("The size of a char: %ld\n", sizeof(name));
     printf("The size of name (char[]): %ld", sizeof(name));
